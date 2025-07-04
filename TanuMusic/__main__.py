d3f7d51fa153e6c6188286a16dcc2e1a3733dec7
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-07-04 22:31:39 +0530

    Update __main__.py

--- TanuMusic/__main__.py
+++ TanuMusic/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await Tanu.decorators()
     LOGGER("TanuMusic").info(
-        "✦ Created By ➥ Kʀɪsʜɴᴇᴛᴡᴏʀᴋ...🥀"
+        "✦ Created By ➥ 𝐀𝐔𝐑𝐀 𝐍ᴇᴛᴡᴏʀᴋ...🧩"
     )
     await idle()
     await app.stop()
